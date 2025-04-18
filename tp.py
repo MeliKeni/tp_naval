f58985eaff7a3248a7fac6a15fffd60bd14c45de
MeliKeni <melinakenigsberg@gmail.com>
2025-04-17 23:43:25 -0300

    definicion de funciones, parte 1 del tp terminada

--- tp.py
+++ tp.py
@@ -1,47 +1,58 @@
 import random
+#delcaracion de funciones
+def crearTablero(n:int) -> list[list[str]]:
+    tablero: list[list[str]] = []
+    for i in range(n):
+        tablero.append([])
+    for i in range(n):
+        for _ in range(n):
+            tablero[i].append(" ")
+    return tablero
+def imprimirTablero(tablero:list[list[str]]) -> None:
+    for fila in tablero:
+        print(fila)
 
-n = 10
-tablero: list[list[str]] = []
+def asignarBarcos(tablero:list[list[str]], cantidadBarcos:int) -> list[list[int]]:
+    posicionesBarcos: list[list[int]] = []
+    for i in range(cantidadBarcos):
+        posicionx = random.randint(0, n-1)
+        posiciony = random.randint(0, n-1)
+        if [posicionx, posiciony] in posicionesBarcos:
+            continue
+        posicionesBarcos.append([posicionx, posiciony])
+        tablero[posicionx][posiciony] = "barco"
+    return posicionesBarcos
 
-for i in range(n):
-    tablero.append([])
+def sistema_disparos(tablero: list[list[str]], posiciones_barcos: list[list[int]]) -> None:
+    cantidadDisparos:int =int(input("Cuantos intentos quiere?"))
+    disparosAcertados:int =0
+    disparosFallados:int =0
+    for i in range(cantidadDisparos):
+        entrada:str = input("Decir posicion barco")
+        intentoDisparo:list[int] = [int(entrada.split(",")[0]), int(entrada.split(",")[1])]
 
-for i in range(n):
-    for _ in range(n):
-        tablero[i].append(" ")
-    print(tablero[i])
+        if intentoDisparo in posicionesBarcos:
+            disparosAcertados += 1
+            posicionesBarcos.remove(intentoDisparo)
+            tablero[intentoDisparo[0]][intentoDisparo[1]] = "golpeado"
+        else :
+            disparosFallados += 1
+    return disparosAcertados, disparosFallados
 
-cantidadBarcos = 2
-posicionesBarcos: list[list[int]] = []
+#creacion y configuarcion del juego
+n= int(input("Tamaño del tablero: "))
+tablero: list[list[str]] = crearTablero(n)
 
-for i in range(cantidadBarcos):
-    posicionx = random.randint(0, n-1)
-    posiciony = random.randint(0, n-1)
-    if [posicionx, posiciony] in posicionesBarcos:
-        continue #chequear que te estaria faltando un barco
-    posicionesBarcos.append([posicionx, posiciony])
+cantidadBarcos = int(input("Cuantos barcos?"))
 
-print(posicionesBarcos)
+posicionesBarcos = asignarBarcos(tablero, cantidadBarcos)
 
-cantidadDisparos:int = 3
-disparosAcertados:int =0 
-disparosFallados:int =0
+print(posicionesBarcos) #me indica donde estan los barcos
+#juego
+disparosAcertados, disparosFallados = sistema_disparos(tablero, posicionesBarcos)
 
-for i in range(cantidadDisparos):
-    entrada:str = input("Decir posicion barco")
-    intentoDisparo:list[int] = [entrada.split(",")[0], entrada.split(",")[1]]
-
-    if intentoDisparo in posicionesBarcos:
-        disparosAcertados += 1
-        posicionesBarcos.remove(intentoDisparo)
-        tablero[intentoDisparo[0]][intentoDisparo[1]] = "golpeado"
-    else :
-        disparosFallados += 1
-
-for i in range(n):
-    for j in range(n):
-        print(tablero[i])
-
+#finalizar el juego
+imprimirTablero(tablero)
 print(disparosAcertados)
 print(disparosFallados)
 
