e6e8d4d620d70991decd22c7e338b642dfe69f1f
MeliKeni <melinakenigsberg@gmail.com>
2025-04-18 18:49:51 -0300

    tp terminado

--- tp.py
+++ tp.py
@@ -15,10 +15,10 @@ def imprimirTablero(tablero:list[list[str]]) -> None:
 def asignarBarcos(tablero:list[list[str]], cantidadBarcos:int) -> list[list[int]]:
     posicionesBarcos: list[list[list[int]]] = []
     
-    for i in range(cantidadBarcos):
-        barco_i: list[list[int]] = []
+    
 
     while len(posicionesBarcos) < cantidadBarcos:
+        barco_i: list[list[int]] = []
         tamanoBarco:int = int(input("Tamaño del barco: "))
         if tamanoBarco > 3:
             print("El barco es de hasta 3 casillas, automaticamente se hara su barco de 3 casillas")
@@ -38,15 +38,13 @@ def asignarBarcos(tablero:list[list[str]], cantidadBarcos:int) -> list[list[int]
         posicionesBarcos.append(barco_i) 
     return posicionesBarcos
 
-def sistema_disparos(tablero: list[list[str]], posiciones_barcos: list[list[list[int]]]) -> None: #aca me gustria poner 2 intes, para que devuleva aciertos y fallos pero no se como hacerlo
-    cantidadDisparos:int =int(input("Cuantos intentos quiere?"))
+def turno(tablero: list[list[str]], posiciones_barcos: list[list[list[int]]]) -> None:
     disparosAcertados:int =0
     disparosFallados:int =0
-    for i in range(cantidadDisparos):
-        entrada:str = input("Decir posicion barco")
-        intentoDisparo:list[int] = [int(entrada.split(",")[0]), int(entrada.split(",")[1])]
+    entrada:str = input("Decir posicion barco")
+    intentoDisparo:list[int] = [int(entrada.split(",")[0]), int(entrada.split(",")[1])]
         
-        for i in range(len(posiciones_barcos)):
+    for i in range(len(posiciones_barcos)):
             if intentoDisparo in posiciones_barcos[i]:
                 disparosAcertados += 1
                 tablero[intentoDisparo[0]][intentoDisparo[1]] = "golpeado"
@@ -56,33 +54,47 @@ def sistema_disparos(tablero: list[list[str]], posiciones_barcos: list[list[list
                 print("Disparo fallado")
     for barco_i in posiciones_barcos:
         hundido:bool = True
-    for casilla in barco_i:
-        x, y = casilla
-        if tablero[x][y] != "golpeado":
-            hundido = False
-    if hundido:
-        print("Barco hundido")
         for casilla in barco_i:
             x, y = casilla
-            tablero[x][y] = "hundido"
+            if tablero[x][y] != "golpeado":
+                hundido = False
+        if hundido:
+            print("Barco hundido")
+            for casilla in barco_i:
+                x, y = casilla
+                tablero[x][y] = "hundido"
     return disparosAcertados, disparosFallados
-
 #creacion y configuarcion del juego
 n= int(input("Tamaño del tablero: "))
-tablero: list[list[str]] = crearTablero(n)
+tableroA: list[list[str]] = crearTablero(n)
+tableroB: list[list[str]] = crearTablero(n)
 
 cantidadBarcos = int(input("Cuantos barcos?"))
 
-posicionesBarcos = asignarBarcos(tablero, cantidadBarcos)
+print("Cree los barcos para el tablero del jugador 1")
+posicionesBarcosA = asignarBarcos(tableroA, cantidadBarcos)
+print("Cree los barcos para el tablero del jugador 2")
+posicionesBarcosB = asignarBarcos(tableroB, cantidadBarcos)
 
+cantidadDisparos:int =int(input("Cuantos intentos quiere?"))
 
-disparosAcertados, disparosFallados = sistema_disparos(tablero, posicionesBarcos)
+disparosAcertadosA:int =0
+disparosFalladosA:int =0
+disparosAcertadosB:int =0
+disparosFalladosB:int =0
+for i in range(cantidadDisparos):
+    print("Turno jugador 1")
+    disparosAcertadosA, disparosFalladosA = turno(tableroA, posicionesBarcosB)
+    print("Turno jugador 2")
+    disparosAcertadosB, disparosFalladosB = turno(tableroB, posicionesBarcosA)
+
 
 #finalizar el juego
-imprimirTablero(tablero)
-print("la cantidad de disparos acertados fueron: ", + disparosAcertados)
-print("la cantidad de disparos fallados fueron: ", + disparosFallados)
-
-
-
-
+print("Tablero jugador 1")
+imprimirTablero(tableroA)
+print("la cantidad de disparos acertados por el jugador 1 fueron: ",  disparosAcertadosA)
+print("la cantidad de disparos fallados por el jugador 1  fueron: ",  disparosFalladosA)
+print("Tablero jugador 2")
+imprimirTablero(tableroB)
+print("la cantidad de disparos acertados por el jugador 1 fueron: ",  disparosAcertadosB)
+print("la cantidad de disparos fallados por el jugador 1  fueron: ",  disparosFalladosB)
