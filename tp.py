9c513575ecaa60a95776173ac64ad0236efbab71
MeliKeni <melinakenigsberg@gmail.com>
2025-04-20 11:42:42 -0300

    comentado

--- tp.py
+++ tp.py
@@ -1,11 +1,13 @@
-import random
+import random #Es una libreria que voy a utilizar en la generacion de barcos para que sea random, esta libreria nos ayuda a elegir numeros aleatorios
 
+#declaro las variables iniciales
 disparosAcertadosA:int =0
 disparosFalladosA:int =0
 disparosAcertadosB:int =0
 disparosFalladosB:int =0
+cantidadBarcos:int =0
 #delcaracion de funciones
-def crearTablero(n:int) -> list[list[str]]:
+def crearTablero(n:int) -> list[list[str]]: #en esta funcion vamos a crear un tablero, asi si quiero jugar de a 2 jugadores puedo usar la funcion 2 veces y listo, basicamente apartir de un entero n te hace una lista de listas, creando asi nuestro tablero de nxn
     tablero: list[list[str]] = []
     for i in range(n):
         tablero.append([])
@@ -13,20 +15,20 @@ def crearTablero(n:int) -> list[list[str]]:
         for _ in range(n):
             tablero[i].append(" ")
     return tablero
-def imprimirTablero(tablero:list[list[str]]) -> None:
+def imprimirTablero(tablero:list[list[str]]) -> None: #esta funcion la vamos a usar para imprimir tableros, basicamente apartir de un tablero (lista de listas) te lo imprime en la consola, prolijo separado por filas
     for fila in tablero:
         print(fila)
 
-def asignarBarcos(tablero:list[list[str]], cantidadBarcos:int) -> list[list[int]]:
+def asignarBarcos(tablero:list[list[str]], cantidadBarcos:int) -> list[list[int]]: #Esta funcion sirve para asignar los barcos en el tablero. Apartir de un tablero, y un entero que represetna la cantidad de barcos que quiero, va preguntando el tamano de los barcos y los va poniendo en posiciones random en el tablero devolviendonos otra lista de listas con las posicones de los tableros guardadas por barco
     posicionesBarcos: list[list[list[int]]] = []
     
     while len(posicionesBarcos) < cantidadBarcos:
         barco_i: list[list[int]] = []
-        tamanoBarco:int = int(input("Tamaño del barco: "))
+        tamanoBarco:int = int(input("Tamaño del barco: ")) #el int antes del input hace que en vez de que te tome lo que escribio el usuario como string lo pase a entero. 
         if tamanoBarco > 3:
             print("El barco es de hasta 3 casillas, automaticamente se hara su barco de 3 casillas")
             tamanoBarco=3
-        posicionx = random.randint(0, n-1)
+        posicionx = random.randint(0, n-1) #estaes la generacion random de las posiciones, se usa el random.randint que te genera un random entero entre los parametros que le ponemos despues
         posiciony = random.randint(0, n-1)
         posicionyfinal= posiciony + tamanoBarco - 1
         if posicionyfinal > n-1:
@@ -42,7 +44,7 @@ def asignarBarcos(tablero:list[list[str]], cantidadBarcos:int) -> list[list[int]
         posicionesBarcos.append(barco_i) 
     return posicionesBarcos
 
-def turno(tablero: list[list[str]], posiciones_barcos: list[list[list[int]]]) -> None:
+def turno(tablero: list[list[str]], posiciones_barcos: list[list[list[int]]]) -> None: #Esta funcion sirve para hacer un turno, es decir, un disparo al tablero, si estuvo acertado o fallado, si hundio un barco, etc. Para esta se ingresa un tablero, y la posciones de los barcos(una lista de listas) apartir de esto le pide al usuario que ingrese una casilla (una poscion del tablero) y chequea si hay barco ahi o no, en caso de que haya chequea si con ese disparo se hunde el barco. 
     disparosAcertados:int =0
     disparosFallados:int =0
     entrada:str = input("Decir posicion barco")
@@ -72,11 +74,11 @@ def turno(tablero: list[list[str]], posiciones_barcos: list[list[list[int]]]) ->
                 x, y = casilla
                 tablero[x][y] = "hundido"
     return disparosAcertados, disparosFallados
-#creacion y configuarcion del juego
+#creamos los tableros
 n= int(input("Tamaño del tablero: "))
 tableroA: list[list[str]] = crearTablero(n)
 tableroB: list[list[str]] = crearTablero(n)
-
+#creamos los barcos
 cantidadBarcos = int(input("Cuantos barcos?"))
 
 print("Cree los barcos para el tablero del jugador 1")
@@ -84,6 +86,7 @@ posicionesBarcosA = asignarBarcos(tableroA, cantidadBarcos)
 print("Cree los barcos para el tablero del jugador 2")
 posicionesBarcosB = asignarBarcos(tableroB, cantidadBarcos)
 
+#aca empieza la partida de forma mas directa, se pregunta la cantidad de disparos que va a tener cada equipo y empiezan aa alternarse disparando
 cantidadDisparos:int =int(input("Cuantos intentos quiere?"))
 
 
@@ -98,7 +101,7 @@ for i in range(cantidadDisparos):
     disparosFalladosB += fallados
 
 
-#finalizar el juego
+#termina el juego, imprimimos lo stableros y cuantos disparos fallo/acerto cada uno
 print("Tablero jugador 1")
 imprimirTablero(tableroA)
 print("la cantidad de disparos acertados por el jugador 1 fueron: ",  disparosAcertadosA)
